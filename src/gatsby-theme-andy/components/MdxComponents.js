7bdcf71ff2d308cc49c092a0bc7934da1be6c8ba
Andy Gao <supagao7@gmail.com>
2020-05-31 15:13:44 -0400

    rebuilding site Sun May 31 15:13:44 EDT 2020

--- src/gatsby-theme-andy/components/MdxComponents.js
+++ src/gatsby-theme-andy/components/MdxComponents.js
@@ -6,15 +6,16 @@ import { Link } from 'gatsby';
 // Animation styles are imported in `src/styles.css`
 
 // TODO cmd+click open page in new tab
+const innerLinkStyles = `px-1 -mx-1 rounded hover:bg-gray-100 focus:bg-blue-100 no-underline`;
 
 const AnchorTag = ({ href, popups = {}, noPopups = false, ...restProps }) => {
   if (!href) href = restProps.to;
   if (!href.match(/^http/))
     return noPopups ? (
-      <Link {...restProps} to={href} />
+      <Link {...restProps} to={href} className={innerLinkStyles}/>
     ) : (
       <Tippy content={popups[href.replace(/^\//, '')]} placement="right" animation="shift-away">
-        <LinkToStacked {...restProps} to={href} />
+        <LinkToStacked {...restProps} to={href} className={innerLinkStyles} />
       </Tippy>
     );
   return noPopups || restProps.children === href ? (
