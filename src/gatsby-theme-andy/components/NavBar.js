1751f2bf90cecc32fabe531058c607a7aaa7d0ec
Andy Gao <supagao7@gmail.com>
2020-05-31 15:22:51 -0400

    rebuilding site Sun May 31 15:22:51 EDT 2020

--- src/gatsby-theme-andy/components/NavBar.js
+++ src/gatsby-theme-andy/components/NavBar.js
@@ -1,10 +1,9 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
 export default function NavBar() {
-  const [open, setOpen] =useState(false);
+  const [open, setOpen] = useState(false);
 
   return (
-  
     <header className=" sm:flex sm:mt-2 mb-2 sm:py-3 sm:items-center">
       <div className="flex items-center justify-between mt-2 py-3 sm:p-0 sm:m-0">
         <div className="title">
@@ -13,32 +12,39 @@ export default function NavBar() {
           </a>
         </div>
         <div className="sm:hidden">
-          <button onClick={() => setOpen(!open)} type="button" className="block text-gray-500 hover:text-black focus:outline-none">
+          <button
+            onClick={() => setOpen(!open)}
+            type="button"
+            className="block text-gray-500 hover:text-black focus:outline-none"
+          >
             <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
-              {open && <path
-              d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-            />}
-              {!open && <path
-                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-              />}
+              {open && (
+                <path d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z" />
+              )}
+              {!open && (
+                <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
+              )}
             </svg>
           </button>
         </div>
       </div>
-      <div className ={open ? 'block sm:flex links' : 'hidden sm:flex links' }>
-        <a className="block sm:inline-block" href="https://andyjgao.com/blog">
+      <div className={open ? 'block sm:flex links' : 'hidden sm:flex links'}>
+        <a className="block sm:inline-block text-gray-700 font-bold" href="https://andyjgao.com/blog">
           Blog
         </a>
-        <a className="block mt-1 sm:mt-0 sm:ml-4" href="https://andyjgao.com/bookshelf">
+        <a
+          className="block mt-1 sm:mt-0 sm:ml-4 text-gray-700 font-bold"
+          href="https://andyjgao.com/bookshelf"
+        >
           Bookshelf
         </a>
-        <a className="block mt-1 sm:mt-0 sm:ml-4" href="https://andyjgao.com/projects">
+        <a className="block mt-1 sm:mt-0 sm:ml-4 text-gray-700 font-bold" href="https://andyjgao.com/projects">
           Projects
         </a>
-        <a className="block mt-1 sm:mt-0 sm:ml-4" href="https://mondaymail.substack.com">
+        <a className="block mt-1 sm:mt-0 sm:ml-4 text-gray-700 font-bold" href="https://mondaymail.substack.com">
           Mail
         </a>
       </div>
     </header>
-  )
+  );
 }
